3b004a5ad97d078b8e3ab70b8af4fc8d7adcd72e
Gashaw Worku <mggashaw@live.com>
2014-12-01 23:06:35 -0800

    Add NavController

---
+++ ShowCase.WebUI/Controllers/NavController.cs
@@ -0,0 +1,33 @@
+﻿using ShowCase.Domain.Abstract;
+using ShowCase.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShowCase.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IThemeRepository repository;
+
+        public NavController()
+        {
+            IThemeRepository t = new EFThemeRepository();
+            repository = t;
+        }
+
+        // GET: /Nav/
+        public PartialViewResult Menu(string name=null)
+        {
+            ViewBag.SelectedName = name;
+            IEnumerable<string> themes = repository.Themes
+                                                   .Select(x => x.Name)
+                                                   .Distinct()
+                                                   .OrderBy(x => x);
+            return PartialView(themes);
+                                                   
+        }
+    }
+}
